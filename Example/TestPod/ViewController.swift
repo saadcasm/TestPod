c02f189acbeaa092996d3e61992f394c36ace484
Saad Sadiq <saad.sadiq@nextgeni.com>
2023-01-10 12:44:34 +0500

    Initial commit

---
+++ Example/TestPod/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  TestPod
+//
+//  Created by Saad Sadiq on 01/10/2023.
+//  Copyright (c) 2023 Saad Sadiq. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
