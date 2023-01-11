63b5d644f68a9d6083a1460692130816fcd1d3f8
Saad Sadiq <saad.sadiq@nextgeni.com>
2023-01-11 10:46:24 +0500

    UPDATE

--- Source/EmailValidator.swift
+++ Source/EmailValidator.swift
@@ -9,12 +9,14 @@ import Foundation
 
 public struct EmailValidator {
     
+    //THIS IS TO VALIDATE EMAIL
     public static func validateEmail(_ email: String) -> Bool {
         let emailRegex = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
         let emailPred = NSPredicate(format: "SELF MATCHES %@", emailRegex)
         return emailPred.evaluate(with: email)
     }
     
+    //THIS IS TO PRINT HELLO WORLD
     public static func sayHello() {
         print("Hello World")
     }
